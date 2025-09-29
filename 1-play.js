bb648b00d531b6a440b510c5c6a671c63389788d
Saulo Santos <166402067+seos-ink@users.noreply.github.com>
2025-09-29 08:21:04 -0300

    teste 2

--- 1-play.js
+++ 1-play.js
@@ -56,16 +56,16 @@ scene("main", () => {
     let speed = 500; // velocidade do jogador
     let BULLET_SPEED = 1500; // velocidade da bala
     var BULLET_enemySPEED = 1000; // velocidade da bala do inimigo
-    
+
 
     onKeyDown("left", () => {
         Player.move(-speed, 0);
     });
     onKeyDown("right", () => {
-        Player.move(speed, 0); 
+        Player.move(speed, 0);
     });
     onKeyDown("down", () => {
-        Player.move(0, speed); 
+        Player.move(0, speed);
     });
     onKeyDown("up", () => {
         Player.move(0, -speed);
@@ -92,26 +92,26 @@ scene("main", () => {
     });
 
     function spawnBullet(p) {
-		add([
-			rect(12, 48),
-			area(),
-			pos(p),
-			anchor("center"),
-			color(255,255,255),
-			outline(4),
-			move(UP, BULLET_SPEED),
-			offscreen({ destroy: true }),
-			// strings here means a tag
-			"bullet",
-		])
-	}
+        add([
+            rect(12, 48),
+            area(),
+            pos(p),
+            anchor("center"),
+            color(255, 255, 255),
+            outline(4),
+            move(UP, BULLET_SPEED),
+            offscreen({ destroy: true }),
+            // strings here means a tag
+            "bullet",
+        ])
+    }
 
     onKeyPress("space", () => {
-		spawnBullet(Player.pos.sub(16, 0))
-		spawnBullet(Player.pos.add(5, 50))
+        spawnBullet(Player.pos.sub(16, 0))
+        spawnBullet(Player.pos.add(5, 50))
         spawnBullet(Player.pos.add(24, 0))
-		
-	})
+
+    })
 
     function spawnEnemyBullet(p) {
         add([
@@ -127,6 +127,37 @@ scene("main", () => {
         ])
     }
 
+    let DarkBulletSpeed = 450;
+    let FinishSpeed = 250;
+
+    function spawnDarkBullet(p) {
+        add([
+            rect(45, 60),
+            area(),
+            pos(p),
+            anchor("center"),
+            color(128, 0, 128),
+            outline(4),
+            move(DOWN, DarkBulletSpeed),
+            offscreen({ destroy: true }),
+            "enemy_Darkbullet",
+        ])
+    }
+
+    function spawnFinish(p) {
+        add([
+            rect(60, 75),
+            area(),
+            pos(p),
+            anchor("center"),
+            color(255, 215, 0),
+            outline(4),
+            move(DOWN, FinishSpeed),
+            offscreen({ destroy: true }),
+            "enemy_Finishbullet",
+        ])
+    }
+
     // function enemyBulletSpawner() {
     //     spawnEnemyBullet(enemyBoss.pos.add(-16, 50))
     //     spawnEnemyBullet(enemyBoss.pos.add(16, 50))
@@ -138,6 +169,17 @@ scene("main", () => {
         spawnEnemyBullet(enemyBoss.pos.add(-16, 50))
         spawnEnemyBullet(enemyBoss.pos.add(16, 50))
     })
+    loop(1.5, () => {
+        if (enemyBoss.hp() <= 700) {
+            spawnDarkBullet(enemyBoss.pos.add(0, -50))
+        }
+    })
+    loop(3.0, () => {
+        if (enemyBoss.hp() <= 500) {
+            spawnFinish(enemyBoss.pos.add(-50, -50))
+            spawnFinish(enemyBoss.pos.add(50, -50))
+        }
+    })
 
     // onKeyPress("e", () => {
     //     spawnBullet(PlayerTwo.pos.sub(16, 0))
@@ -145,26 +187,53 @@ scene("main", () => {
     // })
 
     Player.onCollide("enemy_bullet", () => {
-		destroy(Player)
+        destroy(Player)
         destroyAll(enemyBoss)
-		music.detune = -1200
+        music.detune = -1200
 
-		wait(1, () => {
-			music.paused = true
-			go("stop")
-		})
-	})
+        wait(1, () => {
+            music.paused = true
+            go("stop")
+        })
+    })
 
     Player.onCollide("enemy_bullet", () => {
-
         destroy(Player)
 
         wait(1, () => {
-			music.paused = true
-			go("stop")
-		})
-        
-	})
+            music.paused = true
+            go("stop")
+        })
+
+    })
+
+    Player.onCollide("enemy_Darkbullet", () => {
+        destroy(Player)
+
+        wait(1, () => {
+            music.paused = true
+            go("stop")
+        })
+    })
+
+    Player.onCollide("enemy_Darkbullet", () => {
+        destroy(Player)
+
+        wait(1, () => {
+            music.paused = true
+            go("stop")
+        })
+    })
+
+    onCollide("enemy_bullet", "bullet", (b, eb) => {
+        destroy(b)
+        destroy(eb)
+    })
+
+    onCollide("enemy_Darkbullet", "bullet", (b, eb) => {
+        destroy(b)
+        destroy(eb)
+    })
 
     var ENEMY_SPEED = 500; // velocidade do inimigo
     const BOSS_HEALTH = 1000; // vida do boss
@@ -187,56 +256,56 @@ scene("main", () => {
         ENEMY_SPEED = 700; // aumenta a velocidade do inimigo quando o boss está com menos de 500 de vida      
     }
 
-    
+
 
     enemyBoss.onUpdate((p) => {
-		enemyBoss.move(ENEMY_SPEED * enemyBoss.dir, 0)
-		if (enemyBoss.dir === 1 && enemyBoss.pos.x >= width() - 20) {
-			enemyBoss.dir = -1
-		}
-		if (enemyBoss.dir === -1 && enemyBoss.pos.x <= 20) {
-			enemyBoss.dir = 1
-		}
-	})
+        enemyBoss.move(ENEMY_SPEED * enemyBoss.dir, 0)
+        if (enemyBoss.dir === 1 && enemyBoss.pos.x >= width() - 20) {
+            enemyBoss.dir = -1
+        }
+        if (enemyBoss.dir === -1 && enemyBoss.pos.x <= 20) {
+            enemyBoss.dir = 1
+        }
+    })
 
     enemyBoss.onHurt(() => {
-		Healthbar.set(enemyBoss.hp())
-	})
+        Healthbar.set(enemyBoss.hp())
+    })
 
     onCollide("bullet", "enemy", (b, e) => {
-		destroy(b)
+        destroy(b)
         e.hurt(5)
         shake(10)
-		// addExplode(b.pos, 1, 24, 1)
-	})
+        // addExplode(b.pos, 1, 24, 1)
+    })
 
-	enemyBoss.onDeath(() => {
-		music.stop()
-		go("win")
-	})
+    enemyBoss.onDeath(() => {
+        music.stop()
+        go("win")
+    })
 
-	const Healthbar = add([
-		rect(width(), 24),
-		pos(0, 0),
-		color(107, 201, 108),
-		fixed(),
-		{
-			max: BOSS_HEALTH,
-			set(hp) {
-				this.width = width() * hp / this.max
-				this.flash = true
-			},
-		},
-	])
+    const Healthbar = add([
+        rect(width(), 24),
+        pos(0, 0),
+        color(107, 201, 108),
+        fixed(),
+        {
+            max: BOSS_HEALTH,
+            set(hp) {
+                this.width = width() * hp / this.max
+                this.flash = true
+            },
+        },
+    ])
 
-	Healthbar.onUpdate(() => {
-		if (Healthbar.flash) {
-			Healthbar.color = rgb(255, 255, 255)
-			Healthbar.flash = false
-		} else {
-			Healthbar.color = rgb(127, 255, 127)
-		}
-	})
+    Healthbar.onUpdate(() => {
+        if (Healthbar.flash) {
+            Healthbar.color = rgb(255, 255, 255)
+            Healthbar.flash = false
+        } else {
+            Healthbar.color = rgb(127, 255, 127)
+        }
+    })
 
     // scene("gameOver", () => {
     //     add([
@@ -253,17 +322,17 @@ scene("main", () => {
     //         go("main"); // Reinicia o jogo quando a tecla enter é pressionada
     //     });
 
-    
+
 
 })
 scene("stop", () => {
     add([
-        text("Jogar", 32),
+        text("GAME OVER", 32),
         pos(width() / 2, height() / 2),
         anchor("center"),
     ]);
     add([
-        text("Pressione Enter para Iniciar", 16),
+        text("Pressione Enter para Iniciar", 10),
         pos(width() / 2, height() / 2 + 40),
         anchor("center"),
     ]);
@@ -300,19 +369,19 @@ scene("win", () => {
 
 
 // });
-    // sistema de AI
+// sistema de AI
 
 
-     /*
-    onKeyDown() é uma função que define o que acontece quando uma tecla é pressionada.
-    O primeiro parâmetro é o nome da tecla e o segundo é uma função que define o que acontece quando a tecla é pressionada.
-    up -> tecla para cima
-    down -> tecla para baixo
-    left -> tecla para esquerda 
-    right -> tecla para direita
-    enter -> tecla enter
-    space -> tecla espaço
-    */
+/*
+onKeyDown() é uma função que define o que acontece quando uma tecla é pressionada.
+O primeiro parâmetro é o nome da tecla e o segundo é uma função que define o que acontece quando a tecla é pressionada.
+up -> tecla para cima
+down -> tecla para baixo
+left -> tecla para esquerda 
+right -> tecla para direita
+enter -> tecla enter
+space -> tecla espaço
+*/
 
 
 go('main'); // go é para ir para a cena main
--- testesDaAula/game.js
+++ testesDaAula/game.js
@@ -1,2 +1,47 @@
-kaboom();
+kaboom({
+    fullscreen: true,
+});
+
+loadSprite("player", "../assets/player.png");
+
+setBackground([0, 0, 0]);
+
+add([
+    sprite("player"),
+    area(),
+    anchor("center"),
+    pos(width() / 2, height() / 2),
+])
+
+let bulletSpeed = 400;
+
+onKeyPress("space", () => {
+    function spawnBullet(p) {
+        add([
+            rect(4, 12),
+            pos(p),
+            anchor("center"),
+            color(0, 255, 0),
+            area(),
+            "bullet",
+            move(UP, bulletSpeed),
+        ]);
+    }
+    spawnBullet(Player.pos.sub(16, 0))
+
+});
+
+onKeyDown("left", () => {
+    Player.move(-speed, 0);
+});
+onKeyDown("right", () => {
+    Player.move(speed, 0); 
+});
+onKeyDown("down", () => {
+    Player.move(0, speed); 
+});
+onKeyDown("up", () => {
+    Player.move(0, -speed);
+});
+
 
